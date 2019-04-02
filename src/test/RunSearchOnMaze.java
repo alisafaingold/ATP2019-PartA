7bb938b95e451257a6d2a8ac007d193a8ec8248d
itai dagan <itaidag@post.bgu.ac.il>
2019-04-02 23:13:35 +0300

    refactor Algorithms

--- src/test/RunSearchOnMaze.java
+++ src/test/RunSearchOnMaze.java
@@ -7,26 +7,40 @@ import algorithms.search.*;
 
 import java.util.ArrayList;
 
-/**
- * Created by Aviadjo on 3/22/2017.
- */
 public class RunSearchOnMaze {
     public static void main(String[] args) {
         IMazeGenerator mg = new MyMazeGenerator();
-        Maze maze = mg.generate(40, 40);
-        maze.print();
+        Maze maze = mg.generate(10, 10);
+        //maze.print();
         SearchableMaze searchableMaze = new SearchableMaze(maze);
         /**double sum=0;
-        for(int i=0; i<50; i++){
-            maze = mg.generate(1000, 1000);
-            //maze.print();
-            searchableMaze = new SearchableMaze(maze);
-            sum += (new DepthFirstSearch()).measureAlgorithmTimeMillis(searchableMaze);
-        }
+        for(int i=0; i<30; i++){
+         maze = mg.generate(1000, 1000);
+         //maze.print();
+         searchableMaze = new SearchableMaze(maze);
+         sum += (new BestFirstSearch().measureAlgorithmTimeMillis(searchableMaze));
+         }
         System.out.println(sum/50);**/
+
+        for(int i=0; i<30; i++){
+            maze = mg.generate(30, 30);
+            searchableMaze = new SearchableMaze(maze);
+            BestFirstSearch searcher = new BestFirstSearch();
+            DepthFirstSearch searcher2 = new DepthFirstSearch();
+            Solution solution = searcher.solve(searchableMaze);
+            int bfs = solution.getSolutionPath().size();
+            solution = searcher2.solve(searchableMaze);
+            int dfs = solution.getSolutionPath().size();
+            if(bfs>= dfs)
+                System.out.println(bfs + " vs. " + dfs);
+            else
+                System.out.println("yay :)");
+        }
+
         //solveProblem(searchableMaze, new BreadthFirstSearch());
         /** Average Time - 2373.5 out of 50 Tests **/
-        solveProblem(searchableMaze, new DepthFirstSearch());
+        //solveProblem(searchableMaze, new DepthFirstSearch());
+        /** Average Time - 1022.04 out of 50 Tests **/
         //solveProblem(searchableMaze, new BestFirstSearch());
     }
 
